7ff4f51abcd2b8bbe7846a043c5729ce9271c36c
“[Johann <[johann123.js@gmail.com]>
2019-06-09 10:44:26 +0200

    Working on solving somi issues

--- Reinforcement Learning/gym/cart_pole.py
+++
@@ -1,9 +0,0 @@
-#!/Users/johann/anaconda3/bin/python
-
-import gym
-env = gym.make('CartPole-v0')
-env.reset()
-for _ in range(200):
-    env.render()
-    env.step(env.action_space.sample()) # take a random action
-env.close()
