e955b1bb66843b5a1886922c39eabbed5e15cbcd
“[Johann <[johann123.js@gmail.com]>
2019-06-10 11:47:12 +0200

    still working on the agent

--- Reinforcement Learning/gym/deep_rl.py
+++ Reinforcement Learning/gym/deep_rl.py
@@ -2,56 +2,64 @@
 
 import gym
 import numpy as np
+from collections import deque
+import random
 
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.optimizers import Adam
 
-def start_game(model, env_gym='CartPole-v0', n_episodes=20):
+memory = deque()
+epsilon = 1.0
+batch_size = 64
 
-    env = gym.make('env_gym')
-
-    X_train = []
-    y_train = []
-
-    for i_episode in range(n_episodes):
+def start_game():
+    env = gym.make('CartPole-v0')
+    for i_episode in range(20):
         observation = env.reset()
         for t in range(100):
             #env.render()
-            #print(observation)
-            if not model:
-                action = env.action_space.sample()
-            else:
-                action = model.predict(state)
+            print(observation)
+
+            action = env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(model.predict(state))
+
             observation, reward, done, info = env.step(action)
-            X_train.append(state, observation, reward)
-            #training_data_X.append(reward)
-            y_train.append(action)
+            memory.append(observation)
+            memory.append(action)
+            memory.append(reward)
+            memory.append(done)
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
                 break
     env.close()
 
-# deep learning implementation
-def train_model(X_train, y_train, input_dim=4, activation='relu', lr=0.0001, batch_size=32, epochs=5):
+def build_model():
     model = Sequential()
 
-    model.add(Dense(128, input_dim=input_dim, activation=activation))
-    model.add(Dense(128, activation=activation))
-    model.add(Dropout(0.2))
-
-    model.add(Dense(256, activation=activation))
-    model.add(Dense(256, activation=activation))
-    model.add(Dropout(0.2))
+    model.add(Dense(8, input_dim=4, activation='relu'))
+    model.add(Dense(8, activation='relu'))
+    model.add(Dense(8, activation='relu'))
 
     model.add(Dense(2, activation='softmax'))
+    model.compile(optimizer=Adam(lr=0.001, decay=0.01), loss='mse', metrics=['acc'])
 
-    model.compile(optimizer=Adam(lr=lr), loss='binary_crossentropy', metrics=['accuracy'])
-    model.summary()
+    return model
 
-    model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs)
+def run():
+    start_game()
+    build_model()
 
-#for _ in range(50):
-#    start_game(model, steps=10)
-#    train_model(training_data_X, training_data_X)
-#    model.save("Reinforcement Learning/gym/model/cart_pole.h5")
+    x_batch, y_batch = [], []
+    minibatch = random.sample(
+        memory, min(len(memory), batch_size))
+    for state, action, reward, next_state, done in minibatch:
+        y_target = model.predict(state)
+        y_target[0][action] = reward if done else reward + gamma * np.max(model.predict(next_state)[0])
+        x_batch.append(state[0])
+        y_batch.append(y_target[0])
+
+    model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
+    if epsilon > epsilon_min:
+        epsilon *= epsilon_decay
+
+run()
