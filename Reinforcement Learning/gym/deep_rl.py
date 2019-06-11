b33faaa94835022f66ca31d4bd2b35e6592a5087
“[Johann <[johann123.js@gmail.com]>
2019-06-11 18:15:10 +0200

    changes to deep_rl.py

--- Reinforcement Learning/gym/deep_rl.py
+++ Reinforcement Learning/gym/deep_rl.py
@@ -1,65 +1,40 @@
 #!/Users/johann/anaconda3/bin/python
 
 import gym
-import numpy as np
 from collections import deque
-import random
+import numpy as np
 
 from keras.models import Sequential
-from keras.layers import Dense, Dropout
+from keras.layers import Dense
 from keras.optimizers import Adam
 
-memory = deque()
-epsilon = 1.0
-batch_size = 64
+memory = deque(maxlen=10000)
+env = gym.make('CartPole-v1')
 
-def start_game():
-    env = gym.make('CartPole-v0')
-    for i_episode in range(20):
-        observation = env.reset()
-        for t in range(100):
-            #env.render()
-            print(observation)
+model = Sequential()
 
-            action = env.action_space.sample() if (np.random.random() <= epsilon) else np.argmax(model.predict(state))
+model.add(Dense(8, input_dim=4, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(2, activation='linear'))
 
-            observation, reward, done, info = env.step(action)
-            memory.append(observation)
-            memory.append(action)
-            memory.append(reward)
-            memory.append(done)
-            if done:
-                print("Episode finished after {} timesteps".format(t+1))
-                break
-    env.close()
+model.compile(optimizer=Adam(lr=0.001, decay=0.01), loss='mse', metrics=['acc'])
 
-def build_model():
-    model = Sequential()
-
-    model.add(Dense(8, input_dim=4, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-    model.add(Dense(8, activation='relu'))
-
-    model.add(Dense(2, activation='softmax'))
-    model.compile(optimizer=Adam(lr=0.001, decay=0.01), loss='mse', metrics=['acc'])
-
-    return model
-
-def run():
-    start_game()
-    build_model()
-
-    x_batch, y_batch = [], []
-    minibatch = random.sample(
-        memory, min(len(memory), batch_size))
-    for state, action, reward, next_state, done in minibatch:
-        y_target = model.predict(state)
-        y_target[0][action] = reward if done else reward + gamma * np.max(model.predict(next_state)[0])
-        x_batch.append(state[0])
-        y_batch.append(y_target[0])
-
-    model.fit(np.array(x_batch), np.array(y_batch), batch_size=len(x_batch), verbose=0)
-    if epsilon > epsilon_min:
-        epsilon *= epsilon_decay
-
-run()
+X_train = deque()
+y_train = deque()
+for i_episodes in range(20):
+    state = env.reset()
+    for t in range(100):
+        for i in range(len(memory)):
+            X_train.append(memory[i][1])
+            X_train.append(memory[i][3])
+            y_train.append(memory[i][2])
+        model.fit(np.asarray(X_train), np.asarray(y_train), batch_size=32)
+        # env.render()
+        action = model.predict(observation)
+        observation, reward, done, info = env.step(action)
+        memory.append((state, observation, action, reward, done))
+        if done:
+            print(t)
+            break
+env.close()
